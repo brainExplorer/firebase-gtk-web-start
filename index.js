c9e88c1a4e87df4c01116793e376981f05e5e1ef
brainExplorer <megafalkon@hotmail.com>
2023-07-28 13:14:04 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+// Import stylesheets
+import './style.css';
+// Firebase App (the core Firebase SDK) is always required
+import { initializeApp } from 'firebase/app';
+
+// Add the Firebase products and methods that you want to use
+import {} from 'firebase/auth';
+import {} from 'firebase/firestore';
+
+import * as firebaseui from 'firebaseui';
+
+// Document elements
+const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById('guestbook-container');
+
+const form = document.getElementById('leave-message');
+const input = document.getElementById('message');
+const guestbook = document.getElementById('guestbook');
+const numberAttending = document.getElementById('number-attending');
+const rsvpYes = document.getElementById('rsvp-yes');
+const rsvpNo = document.getElementById('rsvp-no');
+
+let rsvpListener = null;
+let guestbookListener = null;
+
+let db, auth;
+
+async function main() {
+  // Add Firebase project configuration object here
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyCMSze79W8tcT0r32-KHJEz5TYXCRsGd4I",
+  
+    authDomain: "crucial-guard-358714.firebaseapp.com",
+  
+    projectId: "crucial-guard-358714",
+  
+    storageBucket: "crucial-guard-358714.appspot.com",
+  
+    messagingSenderId: "114391750574",
+  
+    appId: "1:114391750574:web:666cf956e6f641c7aada3f",
+  
+    measurementId: "G-6CXR1FMZYF"
+  
+  };
+  // initializeApp(firebaseConfig);
+
+  // FirebaseUI config
+  const uiConfig = {
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    signInOptions: [
+      // Email / Password Provider.
+      EmailAuthProvider.PROVIDER_ID,
+    ],
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        // Handle sign-in.
+        // Return false to avoid redirect.
+        return false;
+      },
+    },
+  };
+
+  // const ui = new firebaseui.auth.AuthUI(auth);
+}
+main();
